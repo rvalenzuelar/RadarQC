4b9485249b42129de08d0736c4d6a38938405749
rvalenzuela <raul.valenzuela@colorado.edu>
2014-12-18 16:52:46 -0700

    added folders

--- main.cpp
+++ main.cpp
@@ -14,11 +14,16 @@ int main () {
 	// The qc object will read from one directory and write to another
 	QString inpath = "/home/raul/Radardata/ppisubset";
 	QString outpath = "/home/raul/Radardata/ppisubset/QCed";
+
+	// QString inpath = "/home/raul/Radardata/p3/case04";
+	// QString outpath = "/home/raul/Radardata/p3/case04/QCed";
+
 	QString suffix = "QC";
 	AirborneRadarQC QC(inpath, outpath, suffix);
 
 	// Process the data
 	QC.processSweeps("ground");
+	// QC.processSweeps("airborne");
 
 	return 0;
 
