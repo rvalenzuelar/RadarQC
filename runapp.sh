063222f8b4ca4cbc4087454c1a3da435de6fcf10
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-03-24 13:40:40 -0600

    new commit

--- runapp.sh
+++ runapp.sh
@@ -4,6 +4,9 @@
 # February, 2015
 
 # creates makefile using qmake
+echo ' '
+echo 'Compiling radarqc ... '
+echo ' '
 qmake -o Makefile AirborneRadarQC.pro
 
 # build radarqc
@@ -14,39 +17,42 @@ INDIR="/home/rvalenzuela/P3/dorade/case04"
 OUTDIR="/home/rvalenzuela/P3/qced/case04"
 
 # run radarqc
+echo ' '
+echo 'Running radarqc ... '
+echo ' '
 ./radarqc $INDIR $OUTDIR
 
 # remove intermedite compiling files
 rm *.o radarqc Makefile
 
-# changes to output directory
-cd $OUTDIR
+# # changes to output directory
+# cd $OUTDIR
 
-# convert qced files to dorade to fix issue
-# with solo3
-echo " "
-echo "Cleaning files with RadxConvert..."
-RadxConvert -f swp* -dorade -outdir $OUTDIR
+# # convert qced files to dorade to fix issue
+# # with solo3
+# echo " "
+# echo "Cleaning files with RadxConvert..."
+# RadxConvert -f swp* -dorade -outdir $OUTDIR
 
-# retrieve RadxConvert output directory
-RDXOUT="$(ls -d */)"
+# # retrieve RadxConvert output directory
+# RDXOUT="$(ls -d */)"
 
-# remove radarqc output files to avoid duplicates
-rm swp*
+# # remove radarqc output files to avoid duplicates
+# rm swp*
 
-# changes to RadxConvert output directory
-cd $RDXOUT
+# # changes to RadxConvert output directory
+# cd $RDXOUT
 
-# add QC suffix
-find $RDXOUT -type f -exec mv '{}' '{}'.QC \;
+# # add QC suffix
+# find $RDXOUT -type f -exec mv '{}' '{}'.QC \;
 
-# move files to QCed directory
-echo "Moving cleaned files to $OUTDIR"
-mv swp* $OUTDIR
+# # move files to QCed directory
+# echo "Moving cleaned files to $OUTDIR"
+# mv swp* $OUTDIR
 
-# goes back to QCed directory and removes RadxConvert
-# output directory
-cd $OUTDIR
-rm -r $RDXOUT
-echo "done"
-echo " "
+# # goes back to QCed directory and removes RadxConvert
+# # output directory
+# cd $OUTDIR
+# rm -r $RDXOUT
+# echo "done"
+# echo " "
