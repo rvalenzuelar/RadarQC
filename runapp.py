c905686fbbeff5452e8289b0ace0a9503f38dc43
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-07-24 17:35:00 -0600

    updated check of system and input args

--- runapp.py
+++ runapp.py
@@ -11,11 +11,19 @@ from datetime import datetime
 
 import os
 import platform
+import sys
 
+startime=datetime.now().strftime('%H:%M:%S')
+
+""" define home directory """
 home = expanduser("~")
 
+""" get system information """
+uname = platform.uname()
+machine=platform.machine()
 
-if platform.machine()=='x86_64':
+""" setup for 64-bit """
+if machine=='x86_64':
 	os.environ['QMAKESPEC']=home+'/miniconda/mkspecs/linux-g++-64'	
 else:
 	os.environ['QMAKESPEC']=home+'/miniconda/mkspecs/linux-g++-32'	
@@ -23,33 +31,40 @@ else:
 
 print "\nCompiling radarqc"
 print "-------------------------------"
-
 if glob('*.o'):
 	for f in glob('*.o'):
 		os.remove(f)
-run_qmake='qmake -o Makefile AirborneRadarQC.pro'
+if uname[1] == 'psdapp.psd.esrl.noaa.gov':
+	qt_project='AirborneRadarQC1.pro'
+else:
+	qt_project='AirborneRadarQC2.pro'
+run_qmake='qmake -o Makefile '+qt_project
 os.system(run_qmake)
 os.system('make')
 
 
 print "\nRunning radarqc"
 print "-------------------------------"
-
 DTMFILE=home+'/Github/RadarQC/merged_dem_38-39_123-124_extended.tif'
-
-INDIR=home+'/P3_v2/dorade/c03/leg14_cor'
-CFACDIR=home+'/P3_v2/cfac/c03/l03_r07_galt_ve'
-LEGTYPE='onshore'
-OUTDIR=home+'/P3_v2/qced_prod/c03/leg14'
-
+INDIR=home+'/P3_v2/dorade/'+sys.argv[1]+'_cor'
+LEGTYPE=sys.argv[2]
+if LEGTYPE=='onshore':
+	CFACDIR=home+'/P3_v2/cfac/c03/l03_r07_galt_ve'	
+elif LEGTYPE=='offshore':
+	CFACDIR=home+'/P3_v2/cfac/c03/l01_r07_galt_ve'	
+OUTDIR=home+'/P3_v2/qced_prod/'+sys.argv[1]
+# OUTDIR=home+'/P3_v2/foo'
 run_radarqc='./radarqc '+INDIR+' '+OUTDIR+' '+CFACDIR+' '+DTMFILE+' '+LEGTYPE
 var=os.system(run_radarqc)
 
-print "\nFinished at:"
-print datetime.now().strftime('%H:%M:%S')
 
+print "\nRunning times:"
+print "-------------------------------"
+print ("Started at: %s"% startime)
+endtime=datetime.now().strftime('%H:%M:%S')
+print ("Finished at: %s"% endtime)
 
-
+""" remove unnecesary files """
 for f in glob('*.o'):
 	os.remove(f)
 os.remove('radarqc')
